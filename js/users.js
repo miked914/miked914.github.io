3f29ad7e537bf48c22130f1a0387a59a7313bda1
miked914 <miked914@ufl.edu>
2016-02-06 04:00:21 -0500

    new commit

--- js/users.js
+++ js/users.js
@@ -1,19 +1,46 @@
-var users = {
+var users = [
 	{	'firstname':'Mo',
 		'lastname':'Sho',
 		'password':'password'
 	},
-	{	'name':'Rickie',
-		'lastname':'Jo',
+	{	'firstname':'Rickie',
+		'lastname':'Joe',
 		'password':'password'
 	}
-};
+];
 
-function addUser(fname, lname, password) {
-	users.push({
-		'name':fname,
-		'lastname':lname,
-		'password':password
-	});
+var usr = $("#login-username");
+var pwd = $("#login-password");
+var loginBTN = $("#btn-login");
+var signupBTN = $("#btn-signup");
 
-}
+// Log in user
+loginBTN.on("click", function(event) {
+	for (var i = 0; i < users.length; i++) {
+        if (users[i].firstname == usr.val()) {
+        	if (users[i].password == pwd.val()) {
+        		usr.val('');
+        		pwd.val('');
+            	alert("You have successfully logged in");
+            }
+        }
+    }
+});
+
+// Sign up user
+signupBTN.on("click", function(event) {
+	users.push(
+		{'firstname':usr.val(),
+		'lastname':'',
+		'password':pwd.val()
+		}
+	);
+
+	alert("You have successfully signed in");
+	console.log("usr: ", usr.val());
+	console.log("pwd: ", pwd.val());
+
+	usr.val('');
+    pwd.val('');
+	
+});
